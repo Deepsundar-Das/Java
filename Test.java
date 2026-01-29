7a893f91522a5819b8baa9a27a39558bc7a6d3a6
Deepsundar Das <deepsundardas8@gmail.com>
2026-01-29 11:00:22 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 class Test{
     void main() {
         System.out.println("Hello Universe");
+        
         var i = 3;
+        for(; i < 10; i+){
+            System.out.print(i + '\t');
+        }
     }
 }
